15fbdf96c65e3f260a399ea643df01f269a7bb98
zlodiak <prozaik81-2@yandex.ru>
2015-06-05 12:58:47 +0300

    redirects ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
     # Before  filters
 
     def signed_in_user
+      store_location
       redirect_to signin_url, notice: "Please sign  in."  unless  signed_in?
     end   
 
